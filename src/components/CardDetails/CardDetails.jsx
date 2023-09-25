78efec24c3594f47e39fdee316799f04bfe7f5fb
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-25 17:37:15 +0600

    install tostify and apply when donate button clicked

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -1,6 +1,10 @@
 import { Link, useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const CardDetails = () => {
+    const notify = () => toast("Donate successfully");
 
     const cards = useLoaderData();
     const { id } = useParams();
@@ -12,12 +16,13 @@ const CardDetails = () => {
             <div className="hero h-[70vh] rounded-xl flex flex-col-reverse items-end" style={{ backgroundImage: `url(${card.image})` }}>
                 <div className="hero-overlay bg-black bg-opacity-50 h-24 lg:h-36 rounded-b-xl">
                     <Link>
-                        <button style={{ backgroundColor: '#FF444A' }} className="text-white p-3 rounded-lg m-5 px-7 lg:m-12 text-xl">Donate ${card.price}</button>
+                        <button onClick={notify} style={{ backgroundColor: '#FF444A' }} className="text-white p-3 rounded-lg m-5 px-7 lg:m-12 text-xl">Donate ${card.price}</button>
                     </Link>
                 </div>
             </div>
-            <h2 className="text-4xl font-bold my-6">{card.title}</h2>
+            <h2 className="text-5xl font-bold mt-10 my-6">{card.title}</h2>
             <p>{card.description}</p>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
